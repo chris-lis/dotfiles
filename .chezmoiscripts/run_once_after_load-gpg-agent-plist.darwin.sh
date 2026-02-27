a1f5800df5f2f2cfd6aed8d466e4e67351979899
Chris Lis <32350102+chris-lis@users.noreply.github.com>
2026-02-27 13:08:36 +0100

    Add GnuPG config & setup scripts

---
+++ .chezmoiscripts/run_once_after_load-gpg-agent-plist.darwin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+# See: https://github.com/drduh/YubiKey-Guide?tab=readme-ov-file#ssh
+echo "Launching gpg-agent plists. Please reboot for changes to take effect."
+launchctl load ~/Library/LaunchAgents/gnupg.gpg-agent.plist
+launchctl load ~/Library/LaunchAgents/gnupg.gpg-agent-symlink.plist
